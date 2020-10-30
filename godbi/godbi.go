5fc551002999bdb6b0017f37733ee38cd76c53cd
Ilya <MstrVLT@users.noreply.github.com>
2020-10-30 15:25:44 +0300

    cleanup

--- godbi/godbi.go
+++ godbi/godbi.go
@@ -5,7 +5,6 @@ import (
 	"encoding/binary"
 	"fmt"
 	"sync"
-	"time"
 
 	"github.com/google/gousb"
 )
@@ -15,6 +14,10 @@ import (
 const (
 	product = 0x3000
 	vendor  = 0x057e
+)
+
+const (
+	dbimagic = "DBI0"
 )
 const (
 	CMD_ID_EXIT       = 0
@@ -33,7 +36,7 @@ type (
 	//     const type = buffer.readUInt32LE(4);
 	//     const id = buffer.readUInt32LE(8);
 	//     const data_size = buffer.readUInt32LE(12);
-	DBICommand struct {
+	DBICmdHeader struct {
 		Magic    [4]byte
 		CMDType  uint32
 		ID       uint32
@@ -53,19 +56,39 @@ type (
 		// Fields for managing async operations
 		waitGroup *sync.WaitGroup
 
-		// Channels for error reporting and closing
+		// Channels for error reporting and
+		logs   chan string
 		errors chan error
 		close  chan bool
 
 		// Channels for various components
 		//commands map[uint32]chan DBICommand
-		read_files map[string]chan uint64
-		size_files map[string]chan uint64
-		files      map[string]chan string // [nsp_name] = path
+		// read_files map[string]chan uint64
+		// size_files map[string]chan uint64
+		files map[string]chan string // [nsp_name] = path
 		// knobs  map[int]chan int
 		// pads   map[int]chan int
 	}
 )
+
+func parseRequestPacket(data []byte) (*DBICmdHeader, error) {
+	if len(data) != 16 {
+		return nil, fmt.Errorf("request packet wrong size: %d != 16", len(data))
+	}
+
+	if string(data[0:4]) != dbimagic {
+		return nil, fmt.Errorf("request wrong packet")
+	}
+
+	packet := &DBICmdHeader{
+		CMDType:  binary.LittleEndian.Uint32(data[4:]),
+		ID:       binary.LittleEndian.Uint32(data[8:]),
+		DataSize: binary.LittleEndian.Uint32(data[12:]),
+	}
+	copy(packet.Magic[:], data[0:4])
+
+	return packet, nil
+}
 
 // Find attempts to locate an AKAI DBIBackend connected via USB and returns
 // a new instance of the DBIBackend type, exposing an API for interacting
@@ -132,12 +155,12 @@ func Find() (*DBIBackend, error) {
 				errors:      make(chan error),
 				close:       make(chan bool),
 				// commands:  make(map[uint32]chan DBICommand),
-				read_files: make(map[string]chan uint64),
-				size_files: make(map[string]chan uint64),
-				files:      make(map[string]chan string),
+				// read_files: make(map[string]chan uint64),
+				// size_files: make(map[string]chan uint64),
+				files: make(map[string]chan string),
 			}
 
-			go mpd.read(time.Nanosecond * 10)
+			go mpd.read()
 			return mpd, nil
 		}
 	}
@@ -162,59 +185,46 @@ func (mpd *DBIBackend) Errors() <-chan error {
 	return mpd.errors
 }
 
-func (mpd *DBIBackend) read(interval time.Duration) {
-	ticker := time.NewTicker(interval)
-	defer ticker.Stop()
-
+func (mpd *DBIBackend) read() {
 	for {
-		// select {
-		// case <-ticker.C:
-		buff := make([]byte, 16)
-		n, err := mpd.inEndpoint.Read(buff)
+		select {
+		case <-mpd.close:
+			return
+		default:
+			buff := make([]byte, 16)
+			n, err := mpd.inEndpoint.Read(buff)
 
-		if err != nil {
-			mpd.errors <- err
-			continue
+			if err != nil {
+				mpd.errors <- err
+				continue
+			}
+
+			header, err := parseRequestPacket(buff[:n])
+
+			if err != nil {
+				mpd.errors <- err
+				continue
+			}
+
+			mpd.handlePacket(*header)
 		}
-
-		data := buff[:n]
-		mpd.waitGroup.Add(1)
-
-		mpd.parseMessage(data)
-
-		// case <-mpd.close:
-		// 	return
 	}
 }
 
-func isCommand(data []byte) bool {
-	return (string(data[0:4]) == "DBI0")
-}
-
-func getCommandType(data []byte) uint32 {
-	return binary.LittleEndian.Uint32(data[4:8])
-}
-
-func getCommand(data []byte) uint32 {
-	return binary.LittleEndian.Uint32(data[8:12])
-}
-
-func (mpd *DBIBackend) responseListCommand(data []byte) {
+func (mpd *DBIBackend) handleCmdList(header DBICmdHeader) {
 	// defer mpd.waitGroup.Done()
 
 	const testStr = "Animal Crossing New Horizons [01006F8002326000][v0].nsp\n"
 
-	fmt.Println(data)
-
-	dataOut := DBICommand{
-		Magic:    [4]byte{68, 66, 73, 48},
+	dataOut := DBICmdHeader{
 		CMDType:  CMD_TYPE_RESPONSE,
-		ID:       getCommand(data),
+		ID:       header.ID,
 		DataSize: uint32(len(testStr)),
 	}
+	copy(dataOut.Magic[:], []byte(dbimagic))
 
 	buf := new(bytes.Buffer)
-	err := binary.Write(buf, binary.LittleEndian, dataOut)
+	err := binary.Write(buf, binary.LittleEndian, &dataOut)
 	if err != nil {
 		fmt.Println(err)
 		return
@@ -225,64 +235,39 @@ func (mpd *DBIBackend) responseListCommand(data []byte) {
 	if err != nil {
 		mpd.errors <- err
 	}
-}
 
-func (mpd *DBIBackend) responseListData(data []byte) {
-	const testStr = "Animal Crossing New Horizons [01006F8002326000][v0].nsp\n"
+	askBuf := make([]byte, 16)
+	n, err := mpd.inEndpoint.Read(askBuf)
 
-	fmt.Println(data)
+	if err != nil {
+		mpd.errors <- err
+	}
 
-	_, err := mpd.outEndpoint.Write([]byte(testStr))
+	askHeader, err := parseRequestPacket(askBuf[:n])
+
+	if err != nil {
+		mpd.errors <- err
+	}
+
+	if askHeader.DataSize != uint32(len(testStr)) {
+		mpd.errors <- fmt.Errorf("ask packet datasize mismatch: ask %d != want %d", askHeader.DataSize, len(testStr))
+	}
+
+	_, err = mpd.outEndpoint.Write([]byte(testStr))
 
 	if err != nil {
 		mpd.errors <- err
 	}
 }
 
-func (mpd *DBIBackend) parseMessage(msg []byte) {
-	defer mpd.waitGroup.Done()
-
-	// Discard invalid messages.
-	if len(msg) != 16 {
-		return
-	}
-
-	if !isCommand(msg) {
-		return
-	}
-
-	// mpd.waitGroup.Add(1)
-	switch getCommandType(msg) {
+func (mpd *DBIBackend) handlePacket(header DBICmdHeader) {
+	switch header.CMDType {
 	case CMD_TYPE_REQUEST:
-		switch getCommand(msg) {
+		switch header.ID {
 		case CMD_ID_LIST_OLD, CMD_ID_LIST:
-			mpd.responseListCommand(msg)
-		}
-	case CMD_TYPE_ACK:
-		switch getCommand(msg) {
-		case CMD_ID_LIST_OLD, CMD_ID_LIST:
-			mpd.responseListData(msg)
+			mpd.handleCmdList(header)
 		}
 	}
-
-	// if !isCommand(msg) {
-	// 	mpd.handleCommand(msg)
-	// 	return
-	// }
-
-	// if isFader(msg) {
-	// 	go mpd.handleFader(msg)
-	// 	return
-	// }
-
-	// if isKnob(msg) {
-	// 	go mpd.handleKnob(msg)
-	// 	return
-	// }
-
-	// if isPad(msg) {
-	// 	go mpd.handlePad(msg)
-	// }
 }
 
 func findDBIBackend(product, vendor uint16) func(desc *gousb.DeviceDesc) bool {
